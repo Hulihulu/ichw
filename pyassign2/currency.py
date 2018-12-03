2656d2e9ec30f9613d5028458c923f8c979aed9f
Hulihulu <43528533+Hulihulu@users.noreply.github.com>
2018-12-03 23:53:47 +0800

    Update currency.py

--- pyassign2/currency.py
+++ pyassign2/currency.py
@@ -1,1 +1,45 @@
 
+#!/usr/bin/env python3
+
+"""currency.py: Currency converter.
+
+__author__ = "Hu Ruihua"
+__pkuid__  = "1800011843"
+__email__  = "1800011843@pku.edu.cn"
+"""
+
+from urllib.request import urlopen
+    
+def exchange(currency_from, currency_to, amount_from):
+    """定义exchange函数
+    """
+    original_url = "http://cs1110.cs.cornell.edu/2016fa/a1server.php?from = "\
+        +currency_from+"&to = "+currency_to+"&amt = "+str(amount_from)
+    
+    doc = urlopen(original_url)
+    docstr = doc.read()
+    doc.close()
+    jstr = docstr.decode("ascii")
+    bool_value = jstr.replace("true","True")
+    jstr = eval(bool_value)
+    
+    return jstr["to"]
+   
+    
+def test_exchange():
+    """测试在网站上用2.5 USD兑换EUR，ARS，ISK的结果
+    """
+    assert "2.1589225 Euros" == exchange("USD","EUR",2.5)
+    assert "37.38225 Argentine Peso" == exchange("USD","ARS",2.5)
+    assert "289.53 Icelandic Króna" == exchange("USD","ISK",2.5)       
+        
+def main():
+    a = input("Please input the currency on hand: ")
+    b = input("Please input the currency to convert to: ")
+    c = str(input("please input amount of currency to convert:"))
+    test_exchange()
+    print(exchange(a,b,c)) 
+
+    
+if __name__ == '__main__':
+    main()
